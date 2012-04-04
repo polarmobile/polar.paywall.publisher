9a9acba062b6b3eb74741354d3989c900e20fdf8
Rishi Ramraj <rishi.ramraj@polarmobile.com>
2012-04-04 15:26:54 -0500

    Exposed setup.main as a console script.

--- server.py
+++ server.py
@@ -26,19 +26,12 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from setuptools import setup
+# This function runs the web server.
+from publisher.server import main
 
-setup(name='polar.paywall.publisher',
-      version='0.1.0rc2',
-      description='Sample implementation of the publisher paywall server.',
-      license='3-Clause BSD',
-      author='Rishi Ramraj',
-      author_email='rishi.ramraj@polarmobile.com',
-      url='http://www.polarmobile.com',
-      packages=['publisher'],
-      install_requires=['setuptools', 'itty', 'simplejson'],
-      extras_require={'test': ['mock']},
-      entry_points='''
-      [console_scripts]
-      paywall.publisher = publisher.server:main
-      ''')
+# This script is included for the convenience of developers when testing the
+# sample implementation. Instead of having to constantly install the server
+# in order to run it, the developer can simply call this file. If this script
+# is called directly, run the web server.
+if __name__ == '__main__':
+    main()
